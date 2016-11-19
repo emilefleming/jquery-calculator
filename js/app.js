b1850395a68c2f78288b608f15d9cb507e1e1de6
Emile Mirth Fleming <emile.fleming@gmail.com>
2016-11-18 19:28:35 -0800

    Tidy up functions

--- js/app.js
+++ js/app.js
@@ -1,133 +1,130 @@
 (function() {
   'use strict';
 
-  const toScreen = function(text) {
-    if(text === 'C') {
-      clear();
-      return;
-    }
-    if ($('#screen').text() === 'ERROR') {
-      return;
-    }
-    if (text === '=') {
-      $('#screen').text(evaluate($('#screen').text()));
-      return;
-    }
-    $('#screen').text($('#screen').text() + text)
-  }
+  const $screen = $('#screen');
 
   const clear = function() {
-    $('#screen').empty();
-  }
+    $screen.empty();
+  };
 
-  const error = function (text) {
-    $('#screen').text('ERROR');
-    // alert(text);
-  }
+  const add = function(op1, op2) {
+    return Number(op1) + Number(op2);
+  };
 
-  const add = function(a, b) {
-    return Number(a) + Number(b);
-  }
+  const subtract = function(op1, op2) {
+    return Number(op1) - Number(op2);
+  };
 
-  const subtract = function(a, b) {
-    return Number(a) - Number(b);
-  }
+  const multiply = function(op1, op2) {
+    return Number(op1) * Number(op2);
+  };
 
-  const multiply = function(a, b) {
-     return Number(a) * Number(b);
-  }
-
-  const divide = function(a, b) {
-    if (b === '0') {
-      alert('bro u cant divide by zero lmao');
+  const divide = function(op1, op2) {
+    if (op2 === '0') {
       return 'ERROR';
     }
-    return Number(a) / Number(b);
-  }
+
+    return Number(op1) / Number(op2);
+  };
 
   const evalChunk = function(chunk) {
-    const a = chunk[1];
-    const b = chunk[3];
+    const op1 = chunk[1];
+    const op2 = chunk[3];
+
     switch (chunk[2]) {
       case '+':
-        return add(a, b);
+        return add(op1, op2);
       case '-':
-        return subtract(a, b);
+        return subtract(op1, op2);
       case 'x':
-        return multiply(a, b);
+        return multiply(op1, op2);
       case '÷':
-        return divide(a, b);
+        return divide(op1, op2);
       default:
-        alert(`Didn't recognize the operator ${chunk[2]}`);
+        return 'ERROR';
     }
-  }
+  };
 
   const eval1 = function(text) {
     let finalResult = text;
-    const chunk = text.match(/(\-?\d*\.?\d+)(\x|\÷)(\-?\d*\.?\d+)/);
+    const chunk = text.match(/(-?\d*\.?\d+)(\x|÷)(-?\d*\.?\d+)/);
+
     if (chunk) {
       const result = evalChunk(chunk);
+
       finalResult = finalResult.replace(chunk[0], result);
       finalResult = eval1(finalResult);
     }
+
     return finalResult;
-  }
+  };
 
   const eval2 = function(text) {
     let finalResult = text;
-    const chunk = text.match(/(\-?\d*\.?\d+)(\+|\-)(\-?\d*\.?\d+)/);
+    const chunk = text.match(/(-?\d*\.?\d+)(\+|-)(-?\d*\.?\d+)/);
+
     if (chunk) {
       const result = evalChunk(chunk);
+
       finalResult = finalResult.replace(chunk[0], result);
       finalResult = eval2(finalResult);
     }
+
     return finalResult;
-  }
-
-
+  };
 
   const evaluate = function(text) {
     const result = eval1(text);
     const finalResult = eval2(result);
+
     return finalResult;
-  }
+  };
+
+  const toScreen = function(text) {
+    if (text === 'C') {
+      clear();
+
+      return;
+    }
+    if ($screen.text() === 'ERROR') {
+      return;
+    }
+    if (text === '=') {
+      $screen.text(evaluate($screen.text()));
+
+      return;
+    }
+    $screen.text($screen.text() + text);
+  };
 
   $('.buttons').on('click', 'span', (event) => {
-    toScreen($(event.target).text())
+    toScreen($(event.target).text());
   });
 
   $('body').keydown(() => {
-    if (event.key === 'Backspace') {
-      $('#screen').text($('#screen').text().slice(0, $('#screen').text().length - 1));
-      return;
-    }
-    if (event.key === 'Enter') {
-      $('#screen').text(evaluate($('#screen').text()));
-      return;
-    }
-    if (event.key === 'Escape') {
-      clear();
-      return;
-    }
-    if (event.key === '+' || event.key === '-') {
+    if (event.key.search(/[0-9]|\.|-|\+/) === 0) {
       toScreen(event.key);
+
       return;
     }
-    if(event.key === '*') {
-      toScreen('x');
-      return;
+    switch (event.key) {
+      case 'Enter':
+        $screen.text(evaluate($screen.text()));
+        break;
+      case 'Escape':
+        $screen.text('');
+        break;
+      case 'Backspace':
+        $screen.text($screen.text().slice(0, -1));
+        break;
+      case '*':
+        toScreen('x');
+        break;
+      case '/':
+        toScreen('÷');
+        break;
+      default:
+        break;
     }
-    if(event.key === '/') {
-      toScreen('÷');
-      return;
-    }
-    if(event.key === '.') {
-      toScreen('.');
-      return;
-    }
-    if (isNaN(event.key)) {
-      return;
-    }
-    toScreen(event.key);
-  })
+  });
 })();
